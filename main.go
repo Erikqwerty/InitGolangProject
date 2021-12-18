62b076ade067cfe7fed908d03cd7b486bbae50b4
erik <colt57@mail.ru>
2021-12-18 17:02:35 +0300

    version:1.1 Init WebProject

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ type application struct {
 func main() {
 	dbtype := flag.String("db", "mysql", "название директории для кода базы данных")
 	Log := flag.Bool("log", true, "Писать логи или нет.")
+	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
